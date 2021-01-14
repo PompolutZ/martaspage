5672c7fa2b68c838bcf5ba88c5d57ad8daf20a68
Oleh Lutsenko <pompolutz@gmail.com>
2021-01-14 21:20:18 +0100

    add tailwindcss, hero image and other assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-    return <div></div>;
+    return (
+        <div className="grid grid-rows-3 grid-cols-1">
+            <div className="bg-gray-900 row-start-1 col-span-full">
+                <img src="/hero.jpg" />
+            </div>
+            <div className="bg-gradient-to-b from-transparent to-gray-900 row-start-1 col-span-full"></div>
+        </div>
+    );
 }
